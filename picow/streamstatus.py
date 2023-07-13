90595190f6e379dedf7a94804a11a82bf9c80886
Simon Prickett <simon@redislabs.com>
2023-07-13 15:59:58 +0100

    Added basic stream status display.

--- picow/streamstatus.py
+++ picow/streamstatus.py
@@ -1,8 +1,13 @@
 from gfx_pack import SWITCH_E
-from picoredis import Redis
 
 import gfx
+import redis
+import secrets
 import time
+
+def refresh_stream_status_display():
+    # TODO
+    return
 
 def run():
     display = gfx.display
@@ -10,17 +15,64 @@ def run():
     gfx.clear_screen()
     gfx.set_backlight(0, 0, 0, 80)
     
-    # TODO connecting to Redis message
-    display.text("TODO: Redis Stream Status", 5, 20, gfx.DISPLAY_WIDTH, 2)
+    # TODO display connecting message...
+    redis_client = redis.connect()
+    # TODO check if we have a client...
+
+    gfx.display_centered("LOADING...", 25, 2)
     display.update()
+
+    # Get consumer group status information...
+    # XINFO GROUPS key
+    response = redis_client.xinfo(
+        "GROUPS",
+        secrets.REDIS_STREAM_KEY
+    )
     
-    # TODO connect to Redis
-    # TODO get stream information...
+    # Response includes all possible consumer groups so need to find the
+    # one we are interested in (secrets.REDIS_CONSUMER_GROUP).  It looks like
+    # this:
+    # [[b'name', b'staff', b'consumers', 1, b'pending', 3, b'last-delivered-id', b'1688993636634-0', b'entries-read', 11, b'lag', 0]]
     
+    if response is None:
+        # TODO error case
+        pass
+    else:
+        found_it = False
+        for consumer_group in response:
+            if consumer_group[1].decode("utf-8") == secrets.REDIS_CONSUMER_GROUP:
+                # Found our consumer group.
+                consumers_in_group = consumer_group[3]
+                jobs_in_progress = consumer_group[5]
+                consumer_lag = consumer_group[11]
+                
+                print(consumers_in_group)
+                print(jobs_in_progress)
+                print(consumer_lag)
+                
+                found_it = True
+                
+                gfx.clear_screen()
+                # TODO set colour by lag...
+                gfx.set_backlight(0, 0, 0, 80)
+                gfx.display_centered("STATUS", 5, 2)
+                display.text(f"CONSUMERS: {consumers_in_group}", 5, 22, gfx.DISPLAY_WIDTH, 1)
+                display.text(f"IN PROGRESS: {jobs_in_progress}", 5, 32, gfx.DISPLAY_WIDTH, 1)
+                display.text(f"LAG: {consumer_lag}", 5, 42, gfx.DISPLAY_WIDTH, 1)
+                gfx.display_centered("E: EXIT", 55, 1)
+                display.update()
+        
+        if not found_it:
+            # TODO consumer group didn't exist!
+            pass
+        
+                
     # TODO periodically update this...
     
     while True:
         time.sleep(0.01)
     
         if gfx.gp.switch_pressed(SWITCH_E):
+            # TODO dispay disconnecting message
+            redis_client.close()
             return
