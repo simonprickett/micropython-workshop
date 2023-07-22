1f17db6766799bcf31139c5a44fe27cf8e318aa6
Simon Prickett <simon@redislabs.com>
2023-07-22 18:56:42 +0100

    Now shows stream data.

--- server/static/app.js
+++ server/static/app.js
@@ -1,6 +1,78 @@
+async function updateStreamStatus () {
+  const STATUS_BASE_CLASSES = 'hero is-bold';
+
+  const headerArea = document.getElementById('headerArea');
+  const statusArea = document.getElementById('statusArea');
+  statusArea.classList.add('is-hidden');
+
+  const updatingMessageArea = document.getElementById('updatingMessage')
+  updatingMessageArea.classList.remove('is-hidden');
+
+
+  const response = await fetch('/status');
+  const statusData = await response.json();
+  let lagClass = 'is-danger';
+
+  if (statusData.lag < 3) {
+    lagClass = 'is-success';
+  } else if (statusData.lag < 6) {
+    lagClass = 'is-info';
+  } else if (statusData.lag < 9) {
+    lagClass = 'is-warning';
+  } else {
+    lagClass = 'is-danger';
+  }
+
+  headerArea.className = `${STATUS_BASE_CLASSES} ${lagClass}`;
+  statusArea.innerHTML = `
+  <div class="card m-4">
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-4">Consumers</p>
+        </div>
+      </div>
+      <div class="content">
+        <p class="is-size-1 has-text-centered">${statusData.consumers}</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="card m-4">
+<div class="card-content">
+  <div class="media">
+    <div class="media-content">
+      <p class="title is-4">In Progress</p>
+    </div>
+  </div>
+  <div class="content">
+    <p class="is-size-1 has-text-centered">${statusData.pending}</p>
+  </div>
+</div>
+</div>
+</div>
+<div class="card m-4">
+<div class="card-content">
+  <div class="media">
+    <div class="media-content">
+      <p class="title is-4">Lag</p>
+    </div>
+  </div>
+  <div class="content">
+    <p class="is-size-1 has-text-centered">${statusData.lag}</p>
+  </div>
+</div>
+</div>
+</div>`;
+
+  statusArea.classList.remove('is-hidden');
+  updatingMessageArea.classList.add('is-hidden');
+};
+
 // Handle click on the carbon intensity button.
 document.getElementById('carbonBtn').onclick = function() {
   window.location.href = '/regional/postcode/OX1';
 };
 
 // TODO handle getting the updates...
+await updateStreamStatus();
